e61f4ee05396bb67277f48c65e6adb6ebfb8a163
Aditya Kanthe <adityakanthe10@gmail.com>
2024-03-09 13:28:35 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Image_Fetcher
+
+1. 
---
+++ script.js
@@ -0,0 +1,9 @@
+const api_url =
+  "https://api.unsplash.com/photos?client_id=6Bfu-CpadHxLBVtuCRajTtjaLWBA8l7RdJqk_0E2Of4";
+
+async function fetchImage() {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+}
